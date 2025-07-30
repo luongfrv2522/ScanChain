7c93f2a028c3dff52535f1aa5faaa96b6f9edcc9
luong-ha <luong.ha@finotech.vn>
2025-07-31 01:10:08 +0700

    1

--- scan-chain-api/src/routers/wallet.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getNativeBalance } from '@app/controllers/wallet.controller';
-
-const router = Router();
-
-router.get('/balance', getNativeBalance);
-
-export default router;
